135eeaddef15f9c002badee3b69b64dcaab1a39f
fabio-codes-win <fabiohc1@hotmail.com>
2023-06-17 11:52:13 -0600

    rest framework

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import yaml
+
+with open('config.yaml', 'r') as file:
+  db_config = yaml.safe_load(file)
+
+db_config = db_config['db_config']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +43,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'restaurant',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +62,7 @@ ROOT_URLCONF = "littlelemon.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ['templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -75,8 +83,16 @@ WSGI_APPLICATION = "littlelemon.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": 'LittleLemon',
+        'USER': db_config['user'],
+        'PASSWORD': db_config['password'],
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': 'SET sql_mode=\'STRICT_TRANS_TABLES\''
+        }
+
     }
 }
 
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+from .models import Booking, Menu
+
+from .serializers import BookingSerializer, MenuSerializer
+
 # Create your views here.
+
+
+def index(request):
+  return render(request, 'index.html', {})
+
+
+class MenuItemView(ListCreateAPIView):
+  queryset = Menu.objects.all()
+  serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+  queryset = Menu.objects.all()
+  serializer_class = MenuSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+  queryset = Booking.objects.all()
+  serializer_class = BookingSerializer
